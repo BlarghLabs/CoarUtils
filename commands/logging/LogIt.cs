27121f7a4d87b530adcc148af3fb678c41d6cb14
Brian Sobel <sobelito@gmail.com>
2019-05-14 14:30:17 +0000

    hsc to logit

--- commands/logging/LogIt.cs
+++ commands/logging/LogIt.cs
@@ -2,6 +2,7 @@
 using CoarUtils.commands.reflection;
 using System;
 using System.Diagnostics;
+using System.Net;
 
 namespace CoarUtils.commands.logging {
   public class LogIt {
@@ -57,6 +58,61 @@ namespace CoarUtils.commands.logging {
         E("error in LogIt|" + ex.Message);
       }
     }
+    public static void Execute(
+      HttpStatusCode hsc,
+      object o,
+      bool removeNewlinesFromMessage = true
+    ) {
+      try {
+        //dont check forever
+        var methodInfo = new StackFrame(1).GetMethod();
+        if (methodInfo.DeclaringType.Name == typeof(LogIt).Name) {
+          methodInfo = new StackFrame(2).GetMethod();
+        }
+        if (methodInfo.DeclaringType.Name == typeof(LogIt).Name) {
+          methodInfo = new StackFrame(3).GetMethod();
+        }
+
+        //for format consistency
+        //https://social.msdn.microsoft.com/Forums/vstudio/en-US/bb926074-d593-4e0b-8754-7026acc607ec/datetime-tostring-colon-replaced-with-period?forum=csharpgeneral
+        var dt = DateTime.UtcNow;
+        var dts = dt.ToString("yyyy-MM-dd HH") + ":" + dt.ToString("mm") + ":" + dt.ToString("ss") + "." + dt.ToString("fff");
+        var s = severity.info;
+        switch (hsc) {
+          case HttpStatusCode.OK:
+            s = severity.info;
+            break;
+          default:
+            s = severity.error;
+            break;
+        }
+        var ss = "[" + s.ToString().ToUpper() + "]";
+        var @class = (methodInfo.DeclaringType == null)
+          ? ""
+          : methodInfo.DeclaringType.Name
+        ;
+        var method = (methodInfo.DeclaringType == null)
+          ? ""
+          : methodInfo.Name
+        ;
+        o = o ?? "";
+        var msg = o.ToString(); //json convert instead?
+        msg = !removeNewlinesFromMessage
+          ? msg
+          : msg.Replace("\r\n", " ").Replace("\n", " ")
+        ; //currently just a string
+        //var whereIAm = WhereAmI.Execute(stepUp: 3);
+
+        //this is for cloud watch logs which requires space after timestamp: http://docs.aws.amazon.com/AWSEC2/latest/WindowsGuide/send_logs_to_cwl.html
+        var space = " ";
+        var log = $"{dts}{space}|{ss}|{@class}|{method}|{msg}";
+
+        LambdaLogger.Log(log);
+      } catch (Exception ex) {
+        E("error in LogIt|" + ex.Message);
+      }
+    }
+
 
     public static void Execute(
       severity s,
