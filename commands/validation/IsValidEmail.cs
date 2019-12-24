9099f84a67c0e814382dce77564b26aec3c06ac9
Brian Sobel <sobelito@gmail.com>
2019-12-24 11:47:35 +0000

    modelstate and email validation

---
+++ commands/validation/IsValidEmail.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Mail;
+using System.Text;
+
+namespace CoarUtils.commands.validation {
+  public class IsValidEmail {
+    public static bool Execute(string email, string displayName = null) {
+      try {
+        if (!string.IsNullOrEmpty(email)){
+          return false;
+        }
+        var ma = new MailAddress(email, displayName, Encoding.UTF8);
+        return true;
+      } catch /* (Exception ex) */ {
+        return false;
+      }
+    }
+  }
+}
---
+++ commands/web/GetModelStateErrors.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace CoarUtils.commands.web {
+  public class GetModelStateErrors {
+    public static List<string> Execute(
+      ModelStateDictionary modelState
+    ) {
+
+      var query = from state in modelState.Values
+                  from error in state.Errors
+                  select error.ErrorMessage;
+
+      var errorList = query.ToList();
+      return errorList;
+    }
+  }
+}
