028a8d39be9f6a5c3d93561b34414d0e146b1987
Brian Sobel <sobelito@gmail.com>
2019-12-24 12:53:46 +0000

    fix

--- commands/validation/IsValidEmail.cs
+++ commands/validation/IsValidEmail.cs
@@ -5,7 +5,7 @@ namespace CoarUtils.commands.validation {
   public class IsValidEmail {
     public static bool Execute(string email, string displayName = null) {
       try {
-        if (!string.IsNullOrEmpty(email)){
+        if (string.IsNullOrEmpty(email)){
           return false;
         }
         var ma = new MailAddress(email, displayName, Encoding.UTF8);
