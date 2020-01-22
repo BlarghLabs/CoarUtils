d07d09d6836869a990a1f93ca485b79d93e2cac8
Brian Sobel <sobelito@gmail.com>
2020-01-22 02:27:28 +0000

    commented out stuff

--- commands/base64/Base64Encode.cs
+++ commands/base64/Base64Encode.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace CoarUtils.commands.base64 {
+﻿namespace CoarUtils.commands.base64 {
   public static class Base64Encode {
     public static string Execute(string plainText) {
       var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
---
+++ commands/hashing/sha256/GetSha256Hash.cs
@@ -0,0 +1,19 @@
+﻿//using System;
+//using System.Security.Cryptography;
+//using System.Text;
+
+//namespace CoarUtils.commands.hashing.sha256 {
+//  public static class GetSha256Hash {
+//    public static string Execute(string value) {
+//      var sb = new StringBuilder();
+//      using (var hash = SHA256.Create()) {
+//        var enc = Encoding.UTF8;
+//        var ba = hash.ComputeHash(enc.GetBytes(value));
+//        foreach (var b in ba)
+//          sb.Append(b.ToString("x2"));
+//      }
+//      var base64 = CoarUtils.commands.base64.Base64Encode.Execute(sb.ToString());
+//      return base64;
+//    }
+//  }
+//}
