6c116c97cd60e503b5e9db5fed2b47c9aef9134b
Brian Sobel <sobelito@gmail.com>
2020-06-17 09:14:56 -0400

    toekn support

--- commands/web/UnauthenticatedUser.cs
+++ commands/web/UnauthenticatedUser.cs
@@ -1,17 +1,45 @@
-﻿using System.Security.Principal;
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
 
 namespace CoarUtils.commands.web {
   public class UnauthenticatedUser {
     public static bool Execute(
       IPrincipal ip
     ) {
-      //ip?.Identity?.Name ?? ""
-      var result =
+      if (
         (ip == null)
-        || (ip.Identity == null)
-        || !ip.Identity.IsAuthenticated
-        || string.IsNullOrEmpty(ip.Identity.Name);
-      return result;
+        ||
+        (ip.Identity == null)
+        ||
+        !ip.Identity.IsAuthenticated
+      ) {
+        return true;
+      }
+
+      var cp = (ClaimsPrincipal)ip;
+      //note: i have seen mult name id and no name
+      var nameId = cp.Claims
+        .Where(x => x.Type.Equals(ClaimTypes.NameIdentifier))
+        .Select(x=>x.Value)
+        .FirstOrDefault()
+      ;
+      var name = cp.Claims
+        .Where(x => x.Type.Equals(ClaimTypes.Name))
+        .Select(x => x.Value)
+        .FirstOrDefault()
+      ;
+      //////{http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier: foo@bar.baz}
+      if (
+        string.IsNullOrEmpty(ip.Identity.Name)
+        &&
+        string.IsNullOrEmpty(name)
+        &&
+        string.IsNullOrEmpty(nameId)
+      ) { 
+        return true;
+      }
+      return false;
     }
 
   }
