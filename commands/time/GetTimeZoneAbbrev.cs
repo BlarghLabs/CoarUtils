6dd6543cf69a9ebc785fdd6c8abc42cae98a603c
Brian Sobel <sobelito@gmail.com>
2020-08-16 00:15:22 -0400

    Update GetTimeZoneAbbrev.cs

--- commands/time/GetTimeZoneAbbrev.cs
+++ commands/time/GetTimeZoneAbbrev.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using System.Linq;
 
 namespace CoarUtils.commands.time {
+
+  /// <summary>
+  /// use http://mj1856.github.io/TimeZoneNames/
+  /// </summary>
   public static class GetTimeZoneAbbrev {
     public static string Execute(string timeZoneId) {
       var tzi = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
-      return tzi.DisplayName;
+      var los = tzi.DisplayName.Split(" ");
+      var hackAbrev = string.Join("", los);
+      return hackAbrev;
     }
   }
 }
