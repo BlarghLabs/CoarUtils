7740cf9012e9dc4b31e452b4408c2f8d1209a939
Brian Sobel <sobelito@gmail.com>
2020-09-08 19:29:35 -0600

    Update GetSha1Hash.cs

--- commands/hashing/sha1/GetSha1Hash.cs
+++ commands/hashing/sha1/GetSha1Hash.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using System.Security.Cryptography;
+using System.Text;
 
 namespace CoarUtils.commands.hashing.sha1 {
   public class GetSha1Hash {
@@ -9,6 +11,21 @@ namespace CoarUtils.commands.hashing.sha1 {
         hash = Convert.ToBase64String(sha1.ComputeHash(ba));
       }
       return hash;
+    }
+
+    //public static string GetHash(string input) {
+    //  return string.Join("", (new SHA1Managed().ComputeHash(Encoding.UTF8.GetBytes(input))).Select(x => x.ToString("X2")).ToArray());
+    //}
+    public static string Execute(string input) {
+      using (var sha1 = new SHA1CryptoServiceProvider()) {
+        var hash = string.Join(
+          "",
+          sha1.ComputeHash(Encoding.UTF8.GetBytes(input))
+            .Select(x => x.ToString("X2"))
+            .ToArray()
+        );
+        return hash;
+      }
     }
   }
 }
