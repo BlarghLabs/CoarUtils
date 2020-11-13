1eaed9e96a7b8269a08b6b56c845c2548c1fc747
Brian Sobel <sobelito@gmail.com>
2020-11-12 23:44:54 -0500

    html stuff

---
+++ commands/strings/RemoveHtmlComments.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace CoarUtils.commands.strings {
+  public class RemoveHtmlComments {
+    public static string Execute(string orig) {
+      return Regex.Replace(orig, "<!--.*?-->", String.Empty, RegexOptions.Singleline);
+    }
+
+  }
+}
---
+++ commands/strings/WhiteSpaceClean.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+using System.Web;
+
+namespace CoarUtils.commands.strings {
+  public static class WhiteSpaceClean {
+    public static string Execute(string orig) {
+      if (string.IsNullOrWhiteSpace(orig)) {
+        return "";
+      }
+      string result = Regex.Replace(HttpUtility.HtmlDecode(orig), @"\t|\n|\r", " ");
+      result = Regex.Replace(result, @"\s+", " ").Trim();
+      return result;
+    }
+  }
+}
