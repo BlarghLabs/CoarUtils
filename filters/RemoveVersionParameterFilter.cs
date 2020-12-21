cd0f4fc2f728f668004bcf7a8421c68cb827cdd2
Brian Sobel <sobelito@gmail.com>
2020-12-21 02:57:42 +0100

    filters

---
+++ filters/RemoveVersionParameterFilter.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Linq;
+
+namespace CoarUtils.filters {
+  public class RemoveVersionParameterFilter : IOperationFilter {
+    public void Apply(OpenApiOperation operation, OperationFilterContext context) {
+      var vp = operation.Parameters.FirstOrDefault(p => p.Name == "version");
+      if (vp != null) {
+        operation.Parameters.Remove(vp);
+      }
+      //var toReplaceWith = new OpenApiPaths();
+      //foreach (var (key, value) in operation.Paths) {
+      //  toReplaceWith.Add(key.Replace("v{version}", operation.Info.Version, StringComparison.InvariantCulture), value);
+      //}
+
+      //operation.Paths = toReplaceWith;
+    }
+  }
+}
