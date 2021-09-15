92b587bc4a63d4a7a9d0bab9b464f19c91cd06cd
Brian Sobel <sobelito@gmail.com>
2021-09-15 05:44:47 -0400

    some html stuff

---
+++ commands/strings/ContainsHTMLElements.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+using HtmlAgilityPack;
+
+namespace CoarUtils.commands.strings {
+  public class ContainsHTMLElements {
+    public static bool Execute(string text) {
+      var doc = new HtmlDocument();
+      doc.LoadHtml(text);
+      return !HtmlIsJustText.Execute(doc.DocumentNode);
+    }
+  }
+}
---
+++ commands/strings/HtmlIsJustText.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+using HtmlAgilityPack;
+
+namespace CoarUtils.commands.strings {
+  public class HtmlIsJustText {
+    public static bool Execute(HtmlNode rootNode) {
+      return rootNode.Descendants().All(n => n.NodeType == HtmlNodeType.Text);
+    }
+  }
+}
