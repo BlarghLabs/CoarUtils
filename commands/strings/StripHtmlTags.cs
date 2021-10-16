8d990433fb06bc28d3f9106b0dc06278e772cb72
Brian Sobel <sobelito@gmail.com>
2021-10-16 17:13:02 -0400

    ++

--- commands/strings/StripHtmlTags.cs
+++ commands/strings/StripHtmlTags.cs
@@ -4,7 +4,7 @@ using System.Text.RegularExpressions;
 namespace CoarUtils.commands.strings {
   //https://stackoverflow.com/questions/18153998/how-do-i-remove-all-html-tags-from-a-string-without-knowing-which-tags-are-in-it
   public class StripHtmlTags {
-    public static string Execute(string input, string replaceWith = String.Empty) {
+    public static string Execute(string input, string replaceWith = "") {
       var result = Regex.Replace(input, "<.*?>", replaceWith);
       return result;
     }
