1b84b2fcf09e6a9ae59e29afe22a5af517942f4e
Brian Sobel <sobelito@gmail.com>
2022-03-22 21:28:26 -0400

    addeded condensed

--- commands/time/GetRelativeTime.cs
+++ commands/time/GetRelativeTime.cs
@@ -2,7 +2,9 @@
 
 namespace CoarUtils.commands.time {
   public static class GetRelativeTime {
-    public static string Execute(DateTime dt) {
+    public static string Execute(
+      DateTime dt
+    ) {
       const int SECOND = 1;
       const int MINUTE = 60 * SECOND;
       const int HOUR = 60 * MINUTE;
@@ -13,7 +15,7 @@ namespace CoarUtils.commands.time {
       var delta = Math.Abs(ts.TotalSeconds);
 
       if (ts.TotalSeconds < 0)
-        return " just now";
+        return "just now";
 
       if (delta < 1 * MINUTE)
         return ts.Seconds == 1 ? "one second ago" : ts.Seconds + " seconds ago";
@@ -66,6 +68,67 @@ namespace CoarUtils.commands.time {
         return "";
       }
       return Execute(dt.Value);
+    }
+
+    public static string ExecuteCondensed(
+      DateTime dt
+    ) {
+      const int SECOND = 1;
+      const int MINUTE = 60 * SECOND;
+      const int HOUR = 60 * MINUTE;
+      const int DAY = 24 * HOUR;
+      const int MONTH = 30 * DAY;
+
+      var ts = new TimeSpan(DateTime.UtcNow.Ticks - dt.Ticks);
+      var delta = Math.Abs(ts.TotalSeconds);
+
+      if (ts.TotalSeconds < 0)
+        return "just now";
+
+      if (delta < 1 * MINUTE)
+        return ts.Seconds == 1 ? "1 sec ago" : ts.Seconds + " sec ago";
+
+      if (delta < 2 * MINUTE)
+        return "1 min ago";
+
+      if (delta < 45 * MINUTE)
+        return ts.Minutes + " min ago";
+
+      if (delta < 90 * MINUTE)
+        return "1 hr ago";
+
+      if (delta < 24 * HOUR)
+        return ts.Hours + " hr ago";
+
+      if (delta < 48 * HOUR)
+        return "yesterday";
+
+      if (delta < 30 * DAY)
+        return ts.Days + " days ago";
+
+      if (delta < 12 * MONTH) {
+        var months = Convert.ToInt32(Math.Floor((double)ts.Days / 30));
+        var monthsDouble = (double)ts.Days / (double)30;
+        return monthsDouble <= 1
+          ? "one mo ago"
+          : (
+            monthsDouble.ToString("n1").EndsWith(".0")
+              ? months + " mo ago"
+              : monthsDouble.ToString("n1") + " mo ago"
+          )
+        ;
+      } else {
+        var years = Convert.ToInt32(Math.Floor((double)ts.Days / 365));
+        var yearsDouble = (double)ts.Days / (double)365;
+        return yearsDouble <= 1
+          ? "one year ago"
+          : (
+            yearsDouble.ToString("n1").EndsWith(".0")
+              ? years + " years ago"
+              : yearsDouble.ToString("n1") + " years ago"
+          )
+        ;
+      }
     }
   }
 }
