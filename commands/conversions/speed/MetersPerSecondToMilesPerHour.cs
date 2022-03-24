dd11f57fdc87e9120d9441a5ae57bd5da945d8b4
Brian Sobel <sobelito@gmail.com>
2022-03-23 22:13:27 -0400

    speed

---
+++ commands/conversions/speed/MetersPerSecondToMilesPerHour.cs
@@ -0,0 +1,9 @@
+﻿namespace CoarUtils.commands.conversions.speed {
+  public class MetersPerSecondToMilesPerHour {
+    public static decimal Execute(decimal metersPerSecond) {
+      //
+      var milesPerHour = metersPerSecond * 2.236936M;
+      return milesPerHour;
+    }
+  }
+}
