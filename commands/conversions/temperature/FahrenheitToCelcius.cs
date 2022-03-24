50167161b2ec6c693c73a7ee516c4041ece1e31e
Brian Sobel <sobelito@gmail.com>
2022-03-23 22:27:00 -0400

    temp

---
+++ commands/conversions/temperature/FahrenheitToCelcius.cs
@@ -0,0 +1,10 @@
+﻿namespace CoarUtils.commands.conversions.temperature {
+  public class FahrenheitToCelcius {
+    public static decimal Execute(decimal f) {
+      var c = (f - 32M) * (5M / 9M);
+      return c;
+    }
+  }
+}
+
+
