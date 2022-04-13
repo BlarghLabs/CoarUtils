be47aa5d1bc630035ee0734031349c9579b66193
Brian Sobel <sobelito@gmail.com>
2022-04-13 18:38:41 -0400

    Create GetFirstDateOfWeek.cs

---
+++ commands/dates/GetFirstDateOfWeek.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace CoarUtils.commands.dates {
+  public class GetFirstDateOfWeek {
+    public static DateTime Execute(
+      int year, 
+      int weekOfYear, 
+      CultureInfo ci
+    ) {
+      DateTime jan1 = new DateTime(year, 1, 1);
+      int daysOffset = (int)ci.DateTimeFormat.FirstDayOfWeek - (int)jan1.DayOfWeek;
+      DateTime firstWeekDay = jan1.AddDays(daysOffset);
+      int firstWeek = ci.Calendar.GetWeekOfYear(jan1, ci.DateTimeFormat.CalendarWeekRule, ci.DateTimeFormat.FirstDayOfWeek);
+      if ((firstWeek <= 1 || firstWeek >= 52) && daysOffset >= -3) {
+        weekOfYear -= 1;
+      }
+      return firstWeekDay.AddDays(weekOfYear * 7);
+    }
+  }
+}
