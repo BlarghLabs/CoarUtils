98f0f0a619902af1a3f9fdece223497705e78ab3
Brian Sobel <sobelito@gmail.com>
2022-11-20 21:36:31 -0500

    Update GetPublicIpAddress.cs

--- commands/web/GetPublicIpAddress.cs
+++ commands/web/GetPublicIpAddress.cs
@@ -2,9 +2,6 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Features;
 using Newtonsoft.Json;
-using System;
-using System.Linq;
-using System.Collections.Generic;
 
 namespace CoarUtils.commands.web {
   public class GetPublicIpAddress {
