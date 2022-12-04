c2f9ccbfebc0f76d3905b6652d8711135e5740d2
Brian Sobel <sobelito@gmail.com>
2022-12-04 16:48:18 -0500

    wip

--- commands/hashing/sha1/GetFileSha1Hash.cs
+++ commands/hashing/sha1/GetFileSha1Hash.cs
@@ -1,7 +1,4 @@
-﻿using System.IO;
-using System.Runtime.Intrinsics.Arm;
-using System.Security.Cryptography;
-using System.Text;
+﻿using System.Security.Cryptography;
 
 namespace CoarUtils.commands.hashing.sha1 {
   public class GetFileSha1Hash {
