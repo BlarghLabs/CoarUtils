54c0e915f14c4b343514e77526dca8c5c8f10ad5
Brian Sobel <sobelito@gmail.com>
2022-12-20 23:53:57 -0500

    Create PoundsToShortTons.cs

---
+++ commands/conversions/weight/PoundsToShortTons.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CoarUtils.commands.conversions.weight {
+  public class PoundsToShortTons {
+
+    public static decimal? Execute(
+      decimal? pounds
+    ) {
+      if (!pounds.HasValue) {
+        return null;
+      }
+
+      //https://www.checkyourmath.com/convert/weight_mass/lb_short_ton.php
+      var result = pounds.Value / 2000M;
+
+      return result;
+    }
+  }
+}
