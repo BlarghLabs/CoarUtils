51bcfcbac97e8421df8603698122806a14c9d9e4
Brian Sobel <sobelito@gmail.com>
2023-03-16 11:15:26 -0600

    Update GetGeographicMidpoint.cs

--- commands/gis/GetGeographicMidpoint.cs
+++ commands/gis/GetGeographicMidpoint.cs
@@ -1,8 +1,5 @@
 ﻿using CoarUtils.commands.logging;
 using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 
 namespace CoarUtils.commands.gis {
@@ -18,21 +15,20 @@ namespace CoarUtils.commands.gis {
       }
       public List<Coordinate> loc { get; set; }
     }
-    public class respone {
-      public respone() {
-      }
+    public class Response {
+      //public Response() {}
       public Coordinate? c { get; set; }
     }
     #endregion
 
 
     public static void Execute(
-      Request m,
-      out respone r,
+      Request request,
+      out Response response,
       out HttpStatusCode hsc,
       out string status
     ) {
-      r = new respone {
+      response = new Response {
         c = new Coordinate { },
       };
       hsc = HttpStatusCode.BadRequest;
@@ -40,27 +36,27 @@ namespace CoarUtils.commands.gis {
 
       try {
         //temp:
-        //m.loc.Add(new Coordinate { lat = 22.9833M, lng = 72.5000M }); //Sarkhej
-        //m.loc.Add(new Coordinate { lat = 18.9750M, lng = 72.8258M }); //Mumbai
-        //m.loc.Add(new Coordinate { lat = 22.3000M, lng = 73.2003M }); //Vadodara
-        //m.loc.Add(new Coordinate { lat = 26.9260M, lng = 75.8235M }); //Jaipur
-        //m.loc.Add(new Coordinate { lat = 28.6100M, lng = 77.2300M }); //Delhi
-        //m.loc.Add(new Coordinate { lat = 22.3000M, lng = 70.7833M }); //Rajkot
+        //request.loc.Add(new Coordinate { lat = 22.9833M, lng = 72.5000M }); //Sarkhej
+        //request.loc.Add(new Coordinate { lat = 18.9750M, lng = 72.8258M }); //Mumbai
+        //request.loc.Add(new Coordinate { lat = 22.3000M, lng = 73.2003M }); //Vadodara
+        //request.loc.Add(new Coordinate { lat = 26.9260M, lng = 75.8235M }); //Jaipur
+        //request.loc.Add(new Coordinate { lat = 28.6100M, lng = 77.2300M }); //Delhi
+        //request.loc.Add(new Coordinate { lat = 22.3000M, lng = 70.7833M }); //Rajkot
 
-        if (m.loc == null || m.loc.Count == 0) {
+        if (request.loc == null || request.loc.Count == 0) {
           status = "no coordinates provided";
           hsc = HttpStatusCode.BadRequest;
           return;
         }
-        if (m.loc.Count == 1) {
-          r.c = m.loc.Single();
+        if (request.loc.Count == 1) {
+          response.c = request.loc.Single();
           hsc = HttpStatusCode.OK;
           status = "only one provided";
           return;
         }
 
         double x = 0, y = 0, z = 0;
-        foreach (var _c in m.loc) {
+        foreach (var _c in request.loc) {
           var latitude = Convert.ToDouble(_c.lat) * Math.PI / 180;
           var longitude = Convert.ToDouble(_c.lng) * Math.PI / 180;
 
@@ -68,14 +64,14 @@ namespace CoarUtils.commands.gis {
           y += Math.Cos(latitude) * Math.Sin(longitude);
           z += Math.Sin(latitude);
         }
-        var total = m.loc.Count;
+        var total = request.loc.Count;
         x = x / total;
         y = y / total;
         z = z / total;
         var centralLongitude = Math.Atan2(y, x);
         var centralSquareRoot = Math.Sqrt(x * x + y * y);
         var centralLatitude = Math.Atan2(z, centralSquareRoot);
-        r.c = new Coordinate {
+        response.c = new Coordinate {
           lat = ((decimal)centralLatitude * 180 / (decimal)Math.PI),
           lng = ((decimal)centralLongitude * 180 / (decimal)Math.PI)
         };
@@ -91,8 +87,8 @@ namespace CoarUtils.commands.gis {
         LogIt.I(JsonConvert.SerializeObject(new {
           hsc,
           status,
-          m,
-          r.c
+          request,
+          response.c
         }));
       }
     }
