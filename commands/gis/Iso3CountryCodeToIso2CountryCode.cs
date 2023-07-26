4fd9c3f9cb2a71486d0f0b1c679e7a212119f955
Brian Sobel <sobelito@gmail.com>
2023-07-26 10:45:52 -0400

    iso3 to iso2

---
+++ commands/gis/Iso3CountryCodeToIso2CountryCode.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace CoarUtils.commands.gis {
+  //https://stackoverflow.com/questions/4884692/converting-country-codes-in-net
+  public static class Iso3CountryCodeToIso2CountryCode {
+    public static string Execute(string iso3CountryCode) {
+      if (string.IsNullOrEmpty(iso3CountryCode)) {
+        return null;
+      }
+      if (iso3CountryCode.Length != 3) {
+        throw new ArgumentException("must be three letters");
+      }
+
+      iso3CountryCode = iso3CountryCode.ToUpper();
+
+      CultureInfo[] cultures = CultureInfo.GetCultures(CultureTypes.SpecificCultures);
+      foreach (CultureInfo culture in cultures) {
+        RegionInfo region = new RegionInfo(culture.LCID);
+        if (region.ThreeLetterISORegionName.ToUpper() == iso3CountryCode) {
+          return region.TwoLetterISORegionName;
+        }
+      }
+
+      return null;
+    }
+  }
+
+}
