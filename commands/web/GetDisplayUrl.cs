c1e8de61e3fdc3e83131d208952e615e0a03e4b2
Brian Sobel <sobelito@gmail.com>
2023-10-03 19:53:43 -0400

    get dispay url

---
+++ commands/web/GetDisplayUrl.cs
@@ -0,0 +1,27 @@
+﻿using CoarUtils.commands.logging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+
+namespace CoarUtils.commands.web {
+  public class GetDisplayUrl {
+    public static string Execute(
+      HttpContext hc
+    ) {
+      //HttpContext.Current.Request.Url.AbsolutePath
+      if (
+        (hc == null)
+        ||
+        (hc.Request == null)
+      ) {
+        return null;
+      }
+      try {
+        return hc.Request.GetDisplayUrl();
+      } catch (Exception ex) {
+        //until I know the behavior
+        LogIt.E(ex);
+        return null;
+      }
+    }
+  }
+}
