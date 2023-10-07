420dd7ee99831c229b5c2839560d7fa3fb7a6041
Brian Sobel <sobelito@gmail.com>
2023-10-07 16:40:48 -0400

    normalize decimal

---
+++ commands/decimals/Normalize.cs
@@ -0,0 +1,13 @@
+﻿namespace CoarUtils.commands.decimals {
+  //https://stackoverflow.com/questions/4525854/remove-trailing-zeros
+  public static class Normalize {
+    //public static decimal Execute(this decimal value) {
+    //  return value / 1.000000000000000000000000000000000m;
+    //}
+
+    public static decimal Execute(decimal value) {
+      return value / 1.000000000000000000000000000000000m;
+    }
+  }
+}
+
