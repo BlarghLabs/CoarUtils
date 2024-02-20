1066bf9692e1f4a473beaf928f61dcfb0b0a112e
Brian Sobel <sobelito@gmail.com>
2024-02-19 19:41:35 -0500

    patch

--- commands/time/ToTimeZoneTime.cs
+++ commands/time/ToTimeZoneTime.cs
@@ -1,8 +1,13 @@
-﻿using System;
-
-namespace CoarUtils.commands.time {
+﻿namespace CoarUtils.commands.time {
   //https://weblog.west-wind.com/posts/2015/feb/10/back-to-basics-utc-and-timezones-in-net-web-apps
   public static class ToTimeZoneTime {
+    public static DateTime Execute(this DateTime dateTime, string timeZoneId = "UTC") {
+      var timeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
+      var offset = (int)timeZoneInfo.BaseUtcOffset.TotalMinutes;
+      return dateTime.AddMinutes(offset);
+    }
+
+    //this has an issue w/ DST
     /// <summary>
     /// Returns TimeZone adjusted time for a given from a Utc or local time.
     /// Date is first converted to UTC then adjusted.
@@ -10,10 +15,10 @@ namespace CoarUtils.commands.time {
     /// <param name="time"></param>
     /// <param name="timeZoneId"></param>
     /// <returns></returns>
-    public static DateTime Execute(this DateTime time, string timeZoneId = "UTC") {
-      TimeZoneInfo tzi = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
-      return time.Execute(tzi);
-    }
+    //public static DateTime Execute(this DateTime time, string timeZoneId = "UTC") {
+    //  TimeZoneInfo tzi = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
+    //  return time.Execute(tzi);
+    //}
 
     /// <summary>
     /// Returns TimeZone adjusted time for a given from a Utc or local time.
@@ -22,8 +27,8 @@ namespace CoarUtils.commands.time {
     /// <param name="time"></param>
     /// <param name="timeZoneId"></param>
     /// <returns></returns>
-    public static DateTime Execute(this DateTime time, TimeZoneInfo tzi) {
-      return TimeZoneInfo.ConvertTimeFromUtc(time, tzi);
-    }
+    //public static DateTime Execute(this DateTime time, TimeZoneInfo tzi) {
+    //  return TimeZoneInfo.ConvertTimeFromUtc(time, tzi);
+    //}
   }
 }
