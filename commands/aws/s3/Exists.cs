bfc1d46d18f6902ef27e7af96c14c4e56d6f4967
Brian Sobel <sobelito@gmail.com>
2024-03-09 23:23:06 -0500

    logging, new async

--- commands/aws/s3/Exists.cs
+++ commands/aws/s3/Exists.cs
@@ -1,12 +1,33 @@
 ﻿using Amazon.S3;
-using CoarUtils;
-using System;
-using System.Threading;
+using Amazon.S3.Model;
+using CoarUtils.commands.logging;
 
 namespace CoarUtils.commands.aws.s3 {
-
-  //TODO: add logging
   public class Exists {
+    public static async Task<bool> Execute(
+      string awsAccessKey,
+      string awsSecretKey,
+      Amazon.RegionEndpoint re,
+      string key,
+      string bucketName,
+      CancellationToken cancellationToken
+    ) {
+      try {
+        using (var amazonS3Client = new AmazonS3Client(awsAccessKey, awsSecretKey, re)) {
+          var listObjectsRequest = new ListObjectsRequest {
+            BucketName = bucketName,
+            Prefix = key,
+            MaxKeys = 1
+          };
+          var listObjectResponse = await amazonS3Client.ListObjectsAsync(listObjectsRequest, cancellationToken);
+          return listObjectResponse.S3Objects.Any();
+        }
+      } catch (Exception ex) {
+        LogIt.E(ex);
+        throw;
+      }
+    }
+
     public static bool Execute(
       string awsAccessKey,
       string awsSecretKey,
@@ -19,12 +40,12 @@ namespace CoarUtils.commands.aws.s3 {
       url = "";
       try {
         using (var s3c = new AmazonS3Client(awsAccessKey, awsSecretKey, re)) {
-          var gomr = s3c.GetObjectMetadataAsync(request: new Amazon.S3.Model.GetObjectMetadataRequest {
+          var getObjectMetadataResponse = s3c.GetObjectMetadataAsync(request: new Amazon.S3.Model.GetObjectMetadataRequest {
             BucketName = bucketName,
             Key = key
           }).Result;
           url = Constants.S3_BASE + bucketName + "/" + key;
-          return gomr.ContentLength != 0;
+          return getObjectMetadataResponse.ContentLength != 0;
           //return true;
         }
       } catch (Amazon.S3.AmazonS3Exception ex) {
@@ -38,7 +59,7 @@ namespace CoarUtils.commands.aws.s3 {
         if (ex.Message.Contains("Error making request with Error Code NotFound and Http Status Code NotFound")) {
           return false;
         }
-        //?
+        LogIt.E(ex);
         throw;
       }
     }
@@ -55,12 +76,12 @@ namespace CoarUtils.commands.aws.s3 {
       contentLength = 0;
       try {
         using (var s3c = new AmazonS3Client(awsAccessKey, awsSecretKey, re)) {
-          var gomr = s3c.GetObjectMetadataAsync(request: new Amazon.S3.Model.GetObjectMetadataRequest {
+          var getObjectMetadataResponse = s3c.GetObjectMetadataAsync(request: new Amazon.S3.Model.GetObjectMetadataRequest {
             BucketName = bucketName,
             Key = key
           }).Result;
-          contentLength = gomr.ContentLength;
-          return gomr.ContentLength != 0;
+          contentLength = getObjectMetadataResponse.ContentLength;
+          return getObjectMetadataResponse.ContentLength != 0;
         }
       } catch (Amazon.S3.AmazonS3Exception ex) {
         if (ex.StatusCode == System.Net.HttpStatusCode.NotFound) {
@@ -75,7 +96,7 @@ namespace CoarUtils.commands.aws.s3 {
           contentLength = 0;
           return false;
         }
-        //?
+        LogIt.E(ex);
         throw;
       }
     }
