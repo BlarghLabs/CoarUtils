fa1116aa6300cbb7eb4437f0741fb5d6ff941725
Brian Sobel <sobelito@gmail.com>
2024-07-08 02:40:48 -0400

    command alternative

--- commands/aws/s3/CreatePresignedUrl.cs
+++ commands/aws/s3/CreatePresignedUrl.cs
@@ -2,11 +2,17 @@
 using Amazon.S3.Model;
 using CoarUtils.commands.logging;
 using CoarUtils.commands.web;
+using CoarUtils.models.commands;
 using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
 using System.Net;
 
 namespace CoarUtils.commands.aws.s3 {
+  #region models
+  public class Response : ResponseStatusModel {
+    public string url { get; set; }
+  }
+  #endregion
 
   //TODO: on fail behavior ... what to do?
   public class CreatePresignedUrl {
@@ -57,6 +63,54 @@ namespace CoarUtils.commands.aws.s3 {
             bucketName,
             key,
             url,
+            numberOfMinutes,
+            ipAddress = GetPublicIpAddress.Execute(hc),
+            executedBy = GetExecutingUsername.Execute()
+          }, Formatting.Indented));
+      }
+    }
+
+    public static Response Execute(
+      string awsAccessKey,
+      string awsSecretKey,
+      string bucketName,
+      string key,
+      Amazon.RegionEndpoint re,
+      CancellationToken cancellationToken,
+      HttpContext hc = null,
+      int numberOfMinutes = 30
+    ) {
+      var response = new Response { };
+      try {
+        using (var s3Client = new AmazonS3Client(awsAccessKey, awsSecretKey, re)) {
+          var gpsur = new GetPreSignedUrlRequest {
+            BucketName = bucketName,
+            Key = key,
+            Expires = DateTime.UtcNow.AddMinutes(numberOfMinutes)
+          };
+          response.url = s3Client.GetPreSignedURL(gpsur);
+        }
+        response.httpStatusCode = HttpStatusCode.OK;
+        return response;
+      } catch (Exception ex) {
+        if (cancellationToken.IsCancellationRequested) {
+          response.httpStatusCode = HttpStatusCode.BadRequest;
+          response.status = Constants.CANCELLATION_REQUESTED_STATUS;
+          return response;
+        }
+
+        LogIt.E(ex);
+        response.httpStatusCode = HttpStatusCode.InternalServerError;
+        response.status = "unexecpected error";
+        return response;
+      } finally {
+        LogIt.I(JsonConvert.SerializeObject(
+          new {
+            response.httpStatusCode,
+            response.status,
+            bucketName,
+            key,
+            response.url,
             numberOfMinutes,
             ipAddress = GetPublicIpAddress.Execute(hc),
             executedBy = GetExecutingUsername.Execute()
