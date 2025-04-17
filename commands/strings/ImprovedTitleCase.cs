718263a4c0772b1f267cd4a87bddbde072169266
Brian Sobel <sobelito@gmail.com>
2025-04-17 17:39:26 -0400

    ImprovedTitleCase

---
+++ commands/strings/ImprovedTitleCase.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace CoarUtils.commands.strings {
+  
+  public static class ImprovedTitleCase {
+    /// <summary>
+    /// this mitigates apostrophe s being 'S
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns></returns>
+
+    public static string Execute(this string input) {
+      if (string.IsNullOrWhiteSpace(input))
+        return input;
+
+      // Use the standard ToTitleCase method first
+      var textInfo = CultureInfo.CurrentCulture.TextInfo;
+      var titleCased = textInfo.ToTitleCase(input.ToLower());
+
+      // Fix apostrophe + s issues by finding patterns like "'S" and converting to "'s"
+      titleCased = Regex.Replace(titleCased, "('S\\b)", "'s");
+
+      return titleCased;
+    }
+  }
+}
