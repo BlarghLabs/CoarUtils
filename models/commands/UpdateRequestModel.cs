d25b0c85830e0798781172a8b90a87e04e136a05
Brian Sobel <sobelito@gmail.com>
2026-02-05 14:49:56 -0700

    new model

---
+++ models/commands/UpdateRequestModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net;
+using CoarUtils.models.commands;
+
+namespace CoarUtils.models.commands {
+  public class UpdateRequestModel {
+    [Required]
+    public long requestTimestamp { get; set; }
+  }
+}
+
+ 
+
+
