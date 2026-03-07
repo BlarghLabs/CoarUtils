f403a692d2105584efbe25abfb2d86f15a1d10ad
Brian Sobel <sobelito@gmail.com>
2026-03-06 19:30:18 -0700

    is valid url

---
+++ commands/validation/IsValidUrl.cs
@@ -0,0 +1,18 @@
+﻿namespace CoarUtils.commands.validation {
+  public class IsValidUrl {
+
+
+    public static bool Execute(string candidateUrl) {
+      if (string.IsNullOrWhiteSpace(candidateUrl)) {
+        return false;
+      }
+     
+      var isValidUrl =
+        Uri.TryCreate(candidateUrl, UriKind.Absolute, out var uriResult)
+        &&
+        (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps)
+      ;
+      return isValidUrl;
+    }
+  }
+}
